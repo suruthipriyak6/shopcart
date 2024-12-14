14974866e94719db748a8405f2aac1df0353a38b
suruthipriyak6 <165206875+suruthipriyak6@users.noreply.github.com>
2024-12-14 16:29:07 -0500

    Add files via upload

---
+++ config/connectDatabase.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_URL).then((con) => {
+        console.log('MongoDB connected to host:'+con.connection.host)
+
+    })
+
+};
+
+module.exports = connectDatabase;
