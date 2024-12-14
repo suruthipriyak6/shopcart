521cc5e32093a4cce1f19d2a32a1f71f6f29e1db
suruthipriyak6 <165206875+suruthipriyak6@users.noreply.github.com>
2024-12-14 16:59:03 -0500

    Add files via upload

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return <footer className="py-1 custom-footer">
+    <p className="text-center text-black mt-1 ">
+        Shop Cart - 2024-2025, All Rights Reserved
+    </p>
+</footer>
+}
+
+
+
