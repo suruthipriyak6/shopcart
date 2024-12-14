fed013d27e0ab70aaf9622f4bfd955b652374c5f
suruthipriyak6 <165206875+suruthipriyak6@users.noreply.github.com>
2024-12-14 16:53:23 -0500

    Delete routes directory

--- routes/order.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createOrder } = require('../controllers/orderController');
-const router = express.Router();
-
-router.route('/order').post(createOrder);
-
-module.exports = router;
